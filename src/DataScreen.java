46acf129ecdd3d1bf3b55682cdc705cbeb6655a6
Huibtr <h.teriet1999@gmail.com>
2020-04-20 10:26:24 +0200

    Add CustomerScreen, OrderScreen and QuantityScreen

--- src/DataScreen.java
+++ src/DataScreen.java
@@ -4,12 +4,39 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class DataScreen extends JFrame implements ActionListener {
+    private JButton jbQuantity;
+    private JButton jbOrders;
+    private JButton jbCustomers;
 
     public DataScreen(){
+        setTitle("Nerdy Gadgets - gegevens inzien/ aanpassen");
+        setSize(500, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(2,1));
+
+        jbQuantity = new JButton("voorraad");
+        jbQuantity.addActionListener(this);
+        add(jbQuantity);
+
+        jbOrders = new JButton("bestelling");
+        jbOrders.addActionListener(this);
+        add(jbOrders);
+
+        jbCustomers = new JButton("klant gegevens");
+        jbCustomers.addActionListener(this);
+        add(jbCustomers);
+
+        setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if(e.getSource() == jbQuantity){
+            QuantityScreen quantityScreen = new QuantityScreen();
+        } else if (e.getSource() == jbOrders){
+            OrderScreen orderScreen = new OrderScreen();
+        } else if(e.getSource() == jbCustomers){
+            CustomerScreen customerScreen = new CustomerScreen();
+        }
     }
 }
