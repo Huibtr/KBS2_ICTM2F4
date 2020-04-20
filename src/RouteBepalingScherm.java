898857401a4c85068103f6a6f9e096d3999782d8
startqwerty <startqwerty@users.noreply.github.com>
2020-04-20 12:18:32 +0200

    wijziging

--- src/RouteBepalingScherm.java
+++ src/RouteBepalingScherm.java
@@ -1,21 +1,23 @@
-import javafx.embed.swing.JFXPanel;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class RouteBepalingScherm extends JFrame {
-    private JLabel route;
-    private JTable tabel;
-    public RouteBepalingScherm(){
-        setTitle("Routebepaling");
-        setSize(500,500);
-        setDefaultCloseOperation(HIDE_ON_CLOSE);
-        setLayout(new FlowLayout(FlowLayout.CENTER));
-        route = new JLabel("Hier komt de route");
-        add(route);
-        tabel = new JTable(3,5);
-        add(tabel);
-        setVisible(true);
+
+        private JLabel route;
+        private JTable tabel;
+        public RouteBepalingScherm(){
+            setTitle("Routebepaling");
+            setSize(500,500);
+            setDefaultCloseOperation(HIDE_ON_CLOSE);
+            setLayout(new FlowLayout(FlowLayout.CENTER));
+            route = new JLabel("Hier komt de route");
+            add(route);
+            tabel = new JTable(3,5);
+            add(tabel);
+            setVisible(true);
+
+
     }
+
 }
