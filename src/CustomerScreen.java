e355975108f39b1debd2eccf88a1a878f02a5587
Huibtr <h.teriet1999@gmail.com>
2020-05-05 11:47:25 +0200

    full table with arrayList

--- src/CustomerScreen.java
+++ src/CustomerScreen.java
@@ -1,87 +1,97 @@
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Vector;
 
 
-public class CustomerScreen extends JFrame implements ActionListener{
+public class CustomerScreen extends JFrame {
+    private  ArrayList<Customer> customers;
     public ResultSet result;
-    private final JButton jbload;
-    private final JTable jtcustomer;
     private final DefaultTableModel tableModel = new DefaultTableModel();
     private JButton jbGoBack;
 
     public CustomerScreen() {
+        getCustomer();
+        JTable table = new JTable();
 
-        setTitle("Nerdy Gadgets - Klantgegevens");
-        setSize(800, 500);
-        setVisible(true);
+        DefaultTableModel model = new DefaultTableModel();
 
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Object[] columnsName = new Object[] {
+                "CustomerID", "CustomerName", "Adress", "Stadsnaam", "Postcode", "Telefoonnummer"
+        };
 
-        jbGoBack = new JButton("< terug");
-        jbGoBack.addActionListener(this);
-        add(jbGoBack, BorderLayout.SOUTH);
+        model.setColumnIdentifiers(columnsName);
 
-        jbload = new JButton("Haal klantgegevens op");
-        jbload.addActionListener(this);
-        add(jbload, BorderLayout.PAGE_START);
+        Object[] rowData = new Object[6];
 
-        jtcustomer = new JTable(tableModel);
-        add(new JScrollPane(jtcustomer), BorderLayout.CENTER);
+        for(int i = 0; i < customers.size(); i++){
 
-    }
+            rowData[0] = customers.get(i).getCustomerID();
+            rowData[1] = customers.get(i).getCustomerName();
+            rowData[2] = customers.get(i).getDeliveryAddressLine1();
+            rowData[3] = customers.get(i).getCityName();
+            rowData[4] = customers.get(i).getDeliveryPostalCode();
+            rowData[5] = customers.get(i).getPhoneNumber();
+            model.addRow(rowData);
+        }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == jbload){
-            System.out.println("refresh data");
-            new SwingWorker<Void, Void>() {
-                @Override
-                protected Void doInBackground() throws Exception {
-                    loadData();
-                    return null;
+        table.setModel(model);
+
+        //add the table to the frame
+        this.add(new JScrollPane(table));
+
+        this.setTitle("Table Example");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+
+        ListSelectionModel modelclick = table.getSelectionModel();
+        modelclick.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent listSelectionEvent) {
+                if (!modelclick.isSelectionEmpty()){
+                    int selectrow = modelclick.getMinSelectionIndex();
+                    JOptionPane.showMessageDialog(null, selectrow);
                 }
-            }.execute();
-            jbload.setText("Refresh klantgegevens");
-        } else if(e.getSource() == jbGoBack) {
-            dispose();
-            DataScreen dataScreen = new DataScreen();
-        }
-    }
-
-    private void loadData() throws SQLException {
-
-        jbload.setEnabled(false);
-
-        DBConnection dbConnection = new DBConnection();
-        ResultSet rs = dbConnection.getCustomers();
-        ResultSetMetaData metaData = rs.getMetaData();
-
-        // naam van de kolommen
-        Vector<String> columnNames = new Vector<>();
-        int columnCount = metaData.getColumnCount();
-        for (int i = 1; i <= columnCount; i++) {
-            columnNames.add(metaData.getColumnName(i));
-        }
-
-        // gegevens uit de data base
-        Vector<Vector<Object>> data = new Vector<>();
-        while (rs.next()) {
-            Vector<Object> vector = new Vector<>();
-            for (int i = 1; i <= columnCount; i++) {
-                vector.add(rs.getObject(i));
             }
-            data.add(vector);
-        }
-
-        tableModel.setDataVector(data, columnNames);
-        jbload.setEnabled(true);
+        });
 
     }
+
+    public void getCustomer(){
+
+        customers = new ArrayList<>();
+        Customer customer;
+        try {
+
+            DBConnection dbConnection = new DBConnection();
+            ResultSet rs = dbConnection.getCustomers();
+
+            while(rs.next()){
+
+                customer = new Customer(
+                        rs.getInt("CustomerID"),
+                        rs.getString("CustomerName"),
+                        rs.getString("DeliveryAddressLine1"),
+                        rs.getString("CityName"),
+                        rs.getString("DeliveryPostalCode"),
+                        rs.getString("PhoneNumber")
+                );
+
+                customers.add(customer);
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
 
 
 }
