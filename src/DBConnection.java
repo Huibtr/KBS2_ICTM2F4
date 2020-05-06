18d2a00f9f80b3035ed8830ddd84d15e0ec2d0d9
Nogikle <57618423+Nogikle@users.noreply.github.com>
2020-05-07 01:00:40 +0200

    QuantityInfoScreen

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -123,6 +123,25 @@ public class DBConnection {
         return result;
     }
 
+    public ResultSet getQuantityInfo(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement query = connection.createStatement();
+
+            result = query.executeQuery("SELECT stockitems.StockItemName, stockitems.StockItemID, stockitemholdings.QuantityOnHand FROM stockitems JOIN stockitemholdings ON stockitems.StockItemID = stockitemholdings.StockItemID;");
+
+        }
+        catch (ClassNotFoundException ex){
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE,null, ex);
+        }
+        catch (SQLException ex){
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE,null, ex);
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
     public ResultSet getCoordinates(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
